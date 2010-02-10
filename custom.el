c64acff30c72762ee5d2b8edb6771859b71918cf
Xinlu <xinlu.h@gmail.com>
2010-02-10 11:33:07 -0500

    Add hook after save to change mod for script files

--- custom.el
+++ custom.el
@@ -706,4 +706,17 @@ replacing matching strings to a specific path"
 
 (setq backup-directory-alist (list (cons "." autosave-dir)))
 
-
+(add-hook 'after-save-hook
+		  #'(lambda ()
+			  (and (save-excursion
+					 (save-restriction
+					   (widen)
+					   (goto-char (point-min))
+					   (save-match-data
+						 (looking-at "^#!"))))
+				   (not (file-executable-p buffer-file-name))
+				   (shell-command (concat "chmod u+x " (shell-quote-argument buffer-file-name)))
+				   (if is-win32
+					   (set-buffer-file-coding-system 'emacs-mule-unix))
+				   (message
+					(concat "Saved as script: " buffer-file-name)))))
